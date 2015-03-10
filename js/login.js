800e55f1bad6a517a8d1c27245f686c8069fa1f5
Alex Studer <alex_studer@outlook.com>
2015-03-10 17:41:47 -0400

    blank field checking

--- js/login.js
+++ js/login.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
 		if ($(this).hasClass("disabled")) {
 			return;
 		}
+		if ($("#username").val() == "" || $("#password").val() == "") {
+			alert("Please type in both a username and password.");
+			return;
+		}
 		$(this).addClass("disabled");
 		$(this).text("Logging in..");
 		window.fclib.setServerURL("forum.dalton.org");
