85a115ded5a42abcaf89ea5675cd7553131c1030
Alex Studer <alex_studer@outlook.com>
2015-03-10 13:34:08 -0400

    Add version info

--- js/fclib.js
+++ js/fclib.js
@@ -6,8 +6,8 @@ fclib.setServerURL = function(url) {
 
 fclib.login = function(username, password, savedPW) {
 	$.get("https://" + window.fclib.server + "/Login/?Templates=JS&NS=1&Containers&Communities&Chats&ThePulse&Watches&People&BFApplications&JSON=2&appplatformtype=2", function(data) {
-		console.log("wooo");
 		console.log(data);
+		alert("Connected to FirstClass server running " + data.versions.OTSW + " with IS version " + data.versions.IS + " and server version " + data.versions.server + "!");
 	});
 };
 
