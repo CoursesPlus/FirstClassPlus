0b0a27cad6d9dca14fec8a1cadcfbbad758a3b3b
Alex Studer <alex_studer@outlook.com>
2015-03-10 17:26:06 -0400

    Basic login

--- js/fclib.js
+++ js/fclib.js
@@ -4,10 +4,25 @@ fclib.setServerURL = function(url) {
 	window.fclib.server = url;
 }
 
-fclib.login = function(username, password, savedPW) {
+fclib.login = function(username, password, savedPW, success, fail) {
 	$.get("https://" + window.fclib.server + "/Login/?Templates=JS&NS=1&Containers&Communities&Chats&ThePulse&Watches&People&BFApplications&JSON=2&appplatformtype=2", function(data) {
 		console.log(data);
 		alert("Connected to FirstClass server running " + data.versions.OTSW + " with IS version " + data.versions.IS + " and server version " + data.versions.server + "!");
+		$.post("https://" + window.fclib.server + "/Login/?Templates=JS&NS=1&Containers&Communities&Chats&ThePulse&Watches&People&BFApplications&JSON=2&appplatformtype=2",
+			{
+				userid: username,
+				password: password,
+				charset: "UTF-8",
+				savedPW: (savedPW ? "1" : "0")
+			},
+			function(data) {
+				console.log(data);
+				if (data.error != undefined) {
+					fail(data.error.str + " [" + data.error.code + "]");
+				} else {
+					success();
+				}
+		});
 	});
 };
 
--- js/login.js
+++ js/login.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 	$("#login").click(function() {
+		if ($(this).hasClass("disabled")) {
+			return;
+		}
+		$(this).addClass("disabled");
+		$(this).text("Logging in..");
 		window.fclib.setServerURL("forum.dalton.org");
-		window.fclib.login("asdf", "asdf");
+		window.fclib.login($("#username").val(), $("#password").val(), false, function() {			
+			// Success
+			alert("login success");
+			$("#login").removeClass("disabled");
+			$("#login").text("Log in");
+		}, function(msg) {			
+			// Fail
+			alert(msg);
+			$("#login").removeClass("disabled");
+			$("#login").text("Log in");
+		});
 	});
 });
